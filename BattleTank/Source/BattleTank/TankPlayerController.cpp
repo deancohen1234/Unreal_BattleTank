2f49ebbdddcf8922da9302ca5a4ce9292db1d1f6
Unknown <dean3712@gmail.com>
2018-06-25 18:46:33 -0600

    Overriding is working well

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,6 +2,23 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay() 
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Tank Player Controller Calling"));
+
+	ATank* tank = GetControlledTank();
+
+	if (tank == nullptr) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank is not possessing any Pawn"));
+	}
+	
+	else 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank is possessing %s"), *tank->GetName());
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
